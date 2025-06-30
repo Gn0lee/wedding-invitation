f4941954f52424d7ba61e13fd802b429fc4bc047
Gn0lee <jinho605@gmail.com>
2025-06-30 23:18:52 +0900

    섹션 컴포넌트 수정

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -8,18 +8,10 @@ export default function Home() {
       <Header />
       <div className="h-full snap-y snap-mandatory overflow-y-auto">
         <MainHero />
-        <Section
-          className="pt-[72px] md:pt-[88px] lg:pt-[104px]"
-          bgColor="bg-blue-100"
-          textColor="text-blue-900"
-        >
+        <Section className="bg-blue-100 text-blue-900">
           <span className="text-2xl font-bold">Section 2</span>
         </Section>
-        <Section
-          className="pt-[72px] md:pt-[88px] lg:pt-[104px]"
-          bgColor="bg-green-100"
-          textColor="text-green-900"
-        >
+        <Section className="bg-green-100 text-green-900 ">
           <span className="text-2xl font-bold">Section 3</span>
         </Section>
       </div>
