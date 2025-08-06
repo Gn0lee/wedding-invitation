a144810a9aab474e135cfc28db4a35cb12fbf175
Gn0lee <jinho605@gmail.com>
2025-08-07 08:57:47 +0900

    fix header

--- next.config.ts
+++ next.config.ts
@@ -67,7 +67,7 @@ const nextConfig: NextConfig = {
               "style-src 'self' 'unsafe-inline' https://fonts.googleapis.com https://ka-f.fontawesome.com",
               "font-src 'self' https://fonts.gstatic.com https://ka-f.fontawesome.com",
               `img-src 'self' data: blob: https: ${process.env.NEXT_PUBLIC_SUPABASE_URL ? `${process.env.NEXT_PUBLIC_SUPABASE_URL.replace('https://', '').replace('http://', '')}` : ''}`,
-              `connect-src 'self' https://www.google-analytics.com https://analytics.google.com https://www.clarity.ms https://t1.kakaocdn.net https://developers.kakao.com https://sharer.kakao.com https://kapi.kakao.com ${process.env.NEXT_PUBLIC_SUPABASE_URL ? `https://${process.env.NEXT_PUBLIC_SUPABASE_URL.replace('https://', '').replace('http://', '')}` : ''} https://*.supabase.co`,
+              `connect-src 'self' https://www.google-analytics.com https://analytics.google.com https://www.clarity.ms https://s.clarity.ms https://t1.kakaocdn.net https://developers.kakao.com https://sharer.kakao.com https://kapi.kakao.com ${process.env.NEXT_PUBLIC_SUPABASE_URL ? `https://${process.env.NEXT_PUBLIC_SUPABASE_URL.replace('https://', '').replace('http://', '')}` : ''} https://*.supabase.co`,
               "frame-src 'self' https://sharer.kakao.com https://developers.kakao.com",
               "object-src 'none'",
               "base-uri 'self'",
